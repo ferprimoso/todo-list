e986ecd70e73a9e0bec0cb4e2aaff834ff02f861
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2022-10-25 20:37:19 -0300

    Add open modal

---
+++ src/makeCard.js
@@ -0,0 +1,9 @@
+class makeCard {
+    constructor(title, description, dueDate, priority, check) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.check = check;
+    }
+}
--- src/startTodoApp.js
+++ src/startTodoApp.js
@@ -1,11 +1,18 @@
 export default function startTodoApp() {
     const body = document.querySelector('body');
 
-
-
     body.appendChild(makeHeader());
     body.appendChild(makeFlexContainer());
     body.appendChild(makeFooter());
+
+    const main = document.querySelector('.main');
+    main.appendChild(makeHome());
+    main.appendChild(makeCard({title: 'clean room', dueDate: '19 nov', description : 'room cleaning bla bla bla i have to', priority:'high', check: false}));
+    main.appendChild(makeCard({title: 'clean room', dueDate: '19 nov', priority:'medium', check: true}));
+    main.appendChild(makeCard({title: 'clean room', dueDate: '19 nov', priority:'low', check: true}));
+
+    body.appendChild(makeForm());
+
 }
 
 function makeHeader() {
@@ -44,8 +51,21 @@ function makeMain() {
     const main = document.createElement('div');
     main.classList.add('main')
 
+    const addButton = document.createElement('button');
+    addButton.classList.add('btn-add');
+    addButton.textContent = '+';
+    addButton.addEventListener('click', () => openModal());
+    main.appendChild(addButton);
+
     return main;
 }
+
+function openModal() {
+    const modal = document.querySelector('.modal');
+
+    modal.classList.add('active');
+}
+
 
 
 function createListElement(text) {
@@ -67,4 +87,98 @@ function makeFooter() {
     footer.appendChild(p);
 
     return footer;
+}
+
+
+function makeHome() {
+    const title = document.createElement('h1');
+    title.classList.add('main-title');
+    title.textContent = 'Home';
+
+
+    return title;
+}
+
+
+function makeCard({title, description, dueDate, priority, check}) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const t = document.createElement('h2');
+    t.classList.add('card-title');
+    t.textContent = title;
+    const c = document.createElement('input');
+    c.type = 'checkbox';
+    c.name = 'check';
+    c.classList.add('checkbox');
+    check === true ? c.checked = true : c.checked = false;
+
+    const divCheckTitle = document.createElement('div');
+    divCheckTitle.appendChild(c);
+    divCheckTitle.appendChild(t);
+
+    const d = document.createElement('h2');
+    d.classList.add('card-date');
+    d.textContent = dueDate;
+
+    const dsc = document.createElement('p');
+    dsc.classList.add('card-description');
+    dsc.textContent = description;
+
+    switch (priority) {
+        case 'high':
+            card.classList.add('red-border');
+            break
+        case 'medium':
+            card.classList.add('yellow-border');
+            break;
+        case 'low':
+            card.classList.add('green-border');
+            break;
+
+
+        default:
+            break;
+    }
+
+    card.appendChild(divCheckTitle);
+    card.appendChild(d);
+
+
+    return card;
+}
+
+function makeForm() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const form = document.createElement('form');
+    form.classList.add('form-container');
+
+    const title = document.createElement('h1');
+    title.textContent = 'Add Task';
+
+    form.appendChild(title);
+    form.appendChild(addFormInput('text','task-name', 'Task Name'));
+    form.appendChild(addFormInput('text','task-description', 'Description'));
+    form.appendChild(addFormInput('date','date', 'Date'));
+
+    const s = document.createElement("input");
+    s.setAttribute("type", "submit");
+    s.setAttribute("value", "Submit");
+    form.appendChild(s);
+
+    modal.appendChild(form);
+
+    return modal;
+}
+
+
+function addFormInput(type, name, placeholder) {
+    const input = document.createElement('input');
+    input.setAttribute('type', type);
+    input.setAttribute('name', name);
+    input.setAttribute('placeholder', placeholder);
+
+    return input;
 }
